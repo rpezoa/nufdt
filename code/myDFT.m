7f5f86b2e4779c2733ab66d0a7616d3a107e6e88
Raquel Pezoa <rpezoa@boson.(none)>
2011-12-07 16:52:18 -0300

    agregando discrete fourier transf.

---
+++ code/myDFT.m
@@ -0,0 +1,27 @@
+function [X] = myDFT(x,t)
+% x: input signal
+N = length(x);
+
+X=zeros(1,N);
+
+
+
+% the k index indicates the frequency and the i index indicates the
+% ith-sample
+if t == 0
+    
+    for k=1:N
+        for i=1:N
+            X(k)= x(i)* exp(-1j*2*pi*(k-1)*(i-1)/N) + X(k);
+        end
+        X(k)=X(k)/N;
+    end
+else
+    for k=1:N
+        for i=1:N
+            X(k)= x(i)* exp(-1j*2*pi*(t(k))*(i-1)/N) + X(k);
+        end
+        X(k)=X(k)/N;
+    end
+end
+    
