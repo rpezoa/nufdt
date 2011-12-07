1bfe31a41af07df988cbe3ccdf56f2f4628e1087
Raquel Pezoa <rpezoarivera@gmail.com>
2011-12-07 16:54:28 -0300

    primera implementacion nufdt

--- code/nudft.m
+++ code/nudft.m
@@ -1,2 +1,26 @@
-function [] = nudft(x)
+function [X] = nudft(x,t)
 % x: input signal
+
+N = length(x);
+
+X=zeros(1,N);
+
+if t == 0
+    for i=1:N
+        t(i)=i-1;
+    end
+end
+
+
+% the k index indicates the frequency and the i index indicates the
+% ith-sample
+
+T=t(N);
+deltaK=2*pi/T;
+
+for k=1:N
+    for i=1:N
+        X(k)= x(i)* exp(-1j*(k-1)*deltaK*t(i)) + X(k);
+    end
+    X(k)=X(k)/N;
+end
