45fb874f0cd8ffc7522091d97908185a0d7ddadc
Raquel Pezoa <rpezoarivera@gmail.com>
2011-12-09 08:49:57 -0300

    modificación de run

--- code/run.m
+++ code/run.m
@@ -1,10 +1,31 @@
-x=[0 1 2 3 4 2 0 2 4 2 0 2 4 3 2 1 0]
-t=[-8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8]
-salida = myDFT(x,t)
-stem(t,x)
+disp('Input Signal :::');
+x=[0 1 2 3 4 2 0 2 4 2 0 2 4 3 2 1 0];
+
+disp('Time :::');
+t=[-8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8];
+
+
+
+
+myDiscreteFourierT = myDFT(x,t);
+discreteFourierT= fft(x);
+
 plot(t,x)
-plot(t,salida)
-plot(t,abs(salida))
-plot(t,real(salida))
+title('Input signal');
+%plot(t,discreteFourierT)
+%plot(t,abs(discreteFourierT))
+
+figure;
+plot(t,real(myDiscreteFourierT));
+title('DFT  calculated with myDFT');
 
 
+figure;
+plot(t,real(discreteFourierT),'r');
+title('DFT  calculated with fft (Matlab)');
+
+
+nuDFT= nudft(x,t);
+figure;
+plot(t,real(nuDFT),'r');
+title('NUDFT  calculated with nudft');
